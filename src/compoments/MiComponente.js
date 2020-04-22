dd5f1959d6fa942962f2e91f85540d2c1284e336
Marcel <halanocca21@gmail.com>
2020-04-22 18:29:28 -0500

    2 formas de empaquetar etiquetas de html y[F uso de bucles

--- src/compoments/MiComponente.js
+++ src/compoments/MiComponente.js
@@ -1,15 +1,63 @@
 
 //  import Objet React and loading modulo react
-import React from 'react';
+import React , {Component} from 'react';
 
-class MiComponente extends React.Component{
+//PRIMERA  FORMA
+//class MiComponente extends React.Component{
+// SEGUNDA FORMA 
+
+class MiComponente extends Component{
 
     //metodo render se encarga de mostrar la vista al usurio
     render(){
 
-        return (
+        let  receta={
+            nombre:'Pizza',
+            ingrediente:['Tomate','Queso','Jamon Cocido','Pan'],
+            calorias:400
+        };
 
-            <h1>Hola , Soy el Componete llamado MiConponente</h1>
+        return (
+            //  LO NORMAL
+           // <h1>Hola , Soy el Componete llamado MiConponente</h1>
+
+            //  DOS FORMAS DE DE IMPRIMIR ETIQUTAS HTML DE 2 A MAS 
+            //PRIMER FORMA
+           /*  <React.Fragment>
+                <h1>Hola , Soy el Componete llamado MiConponente</h1>
+                <h2> Estoy Probando el  componente</h2>
+            <hr/>
+           
+            </React.Fragment> */
+            //SEGUNDA FORMA
+            <div className="mi-componente">
+                <h1>Hola , Soy el Componete llamado MiConponente</h1>
+                <h2> Estoy Probando el  componente</h2>
+                <hr/>
+                
+                <h1> INGREDIENTES PARA UN SANWCHES </h1>
+                <h1>{'Receta   : ' + receta.nombre }</h1>
+                <h2>{'Calorias : ' + receta.calorias}</h2>
+
+                <ol>
+                    {
+                    receta.ingrediente.map((ingrediente,i)=>{
+
+                        console.log(ingrediente);
+
+                        return (
+                            <li key={i}>
+                                {ingrediente}
+                            </li>
+                        )
+
+                    })
+                    }
+                </ol>
+
+
+            </div>
+
         );
     }
 }
