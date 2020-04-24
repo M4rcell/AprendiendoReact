c562163788d656d0e0b660a7e2c08d860863024a
Marcel <halanocca21@gmail.com>
2020-04-24 18:42:04 -0500

    Estado de lo componentes (utilizacion de React State)

--- src/compoments/seccionPruebas.js
+++ src/compoments/seccionPruebas.js
@@ -5,53 +5,106 @@ import React, { Component } from "react";
 import MiComponete from "./MiComponente";
 import Peliculas from "./peliculas";
 
-
 class SeccionPrueba extends Component {
+
+   // Declaracion de Propiedades 
+   contador = 0;
+
+// DECLARACION DE STATE LARGO
+/*    constructor(props){
+
+    super(props);
+
+    this.state={
+
+        contador:0
+
+    };
+   } */
+   // DECLARACION DE STATE CORTA
+
+state={
+    contador:0
+
+};
 
   render() {
     var nombre = "Pepe Man";
     var edad = 20;
-    
+
     return (
       <section id="content">
-          <h2 className="subheader" > Ultimos Articulos</h2>
+        <h2 className="subheader"> Ultimos Articulos</h2>
 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> Pagina principal de aprendiendo react js</p>
-
+        
+        <h2 className="subheader"> Funciones de JSX Basico</h2>
         {this.HolaMan(nombre, edad)}
 
+        <h2 className="subheader"> Componentes</h2>
         <section className="Componentes">
           <MiComponete />
 
           <Peliculas />
         </section>
+
+        <h2 className="subheader"> Estado</h2>
+     
+        <p>contador : {this.state.contador}</p>
+
+        <p>
+            <input type="button" value="Sumar" onClick={this.sumar.bind(this)}/>
+            <input type="button"  value="Restar"  onClick={this.restar.bind(this)}/>
+        </p>
+
       </section>
     );
   }
 
-//Funcion Fecha HolaMan=()=>{}
+  //Funcion Fecha HolaMan=()=>{}
 
-   HolaMan(nombre, edad) {
+  HolaMan(nombre, edad) {
     // var presentacion =
     //   <div>
     //     <h2>Hola Hola soy : {nombre}</h2>
     //     <h3>Tengo : {edad} años</h3>
     //   </div>
-  
+
     // SEGUNDA FORMA
-  
+
     var presentacion = (
       <div>
         <h2>Hola Hola soy : {nombre}</h2>
         <h3>Tengo : {edad} años</h3>
       </div>
     );
-  
+
     return presentacion;
   }
+  
+  sumar(){
+     
+    //this.contador=this.contador+1;
+    this.setState({
+        contador:(this.state.contador+1)
+    });
+
+  }
+
+  restar(){
+   // this.contador=this.contador-1;
+
+   this.setState({
+    contador:(this.state.contador-1)
+   });
+
+
+  }
+
+  
 }
 
 export default SeccionPrueba;
